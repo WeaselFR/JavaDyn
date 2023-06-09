878968bdccf5002b9a50d4ede9d01356cf311561
WeaselFR <benoit.rogeau@laposte.net>
2023-06-09 16:18:54 +0200

    V60

--- assets/js/home.js
+++ assets/js/home.js
@@ -28,8 +28,6 @@ function handleError(error) {
   }
 }
 
-// Déplacer la fonction isLogin() ici avant son utilisation
-
 async function fetchData(url) {
   const res = await fetch(url);
   if (!res.ok) {
